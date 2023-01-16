4b2ed9d942d3f9e6ccec63ac1be24fcd85e09ecb
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-01-16 14:04:57 -0600

    adding functionality to the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,66 @@ let apiKey = '3911b3545970347c6b98f6c645c609f0';
 
 let inputEl = document.getElementById("searchField");
 let searchBtn = document.getElementsByClassName("form-btn");
-let searchHistory = document.getElementById('search-history'); 
-recordCityData()
-// storing seach history in localStorage. 
- let cityName = localStorage.getItem('search-history');
+let searchHistory = document.getElementById('search-history');
+
+// getting date
+
+const date = new Date();
+var dateString = date.toLocaleDateString();
 
 // URL for current day parameters (city name + weather units of measurements)
 
-let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial' + apiKey;
+let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
 
-let forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial' + apiKey;
+let forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial&appid=' + apiKey;
+
+// get search history in local storage
+function getSearchHistory() {
+    return localStorage.getItem('cityHistory');
+}
 
 // Sets the input value in localStorage
-function recordCityData() {
-    localStorage.setItem(searchHistory  , inputEl.value);
+function setSearchHistory(city) {
+    let history = getSearchHistory()
+    let parseHistory;
+    if (history) {
+        // parse history
+        parseHistory = JSON.parse(history);
+
+    } else {
+        parseHistory = []
+    }
+
+    if (!parseHistory.inludes(city)) {
+        parseHistory.shift(city);
+        window.localStorage.setItem('cityHistory', JSON.stringify(parseHistory))
+    }
 }
 
 // Append the search input from localStorage to the cities list
-for (var i = 0; i < localStorage.length; i++) {
-    searchHistory.append( localStorage.getItem(localStorage.key(i)));
-    searchHistory.textContent = cityName;
+function displayHistory() {
+    let history = getSearchHistory()
+
+    if (history) {
+        let parseHistory = JSON.parse(history);
+        for (let i = 0; i < parseHistory.length; i++) {
+            var liEl = document.createElement('li');
+            liEl.textContent = parseHistory[i];
+            searchHistory.append(liEl);
+        }
+    }
+
 }
+
+searchBtn.addeventlistener('click', function(e){
+    e.preventDefault();
+    
+    var city = inputEl.value;
+
+    // set city to searchHistory 
+
+    // display search history, 
+    // clear input
+    // get weather data.
+    
+})
