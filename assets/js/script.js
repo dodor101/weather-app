2f803ea7c0c69b20bf63130425f6f3e7374c2d3e
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-01-17 02:00:35 -0600

    Appended content to the first day of forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,90 @@ let apiKey = '3911b3545970347c6b98f6c645c609f0';
 let inputEl = document.getElementById("searchField");
 let searchBtn = document.getElementById("form-btn");
 let searchHistory = document.getElementById('search-history');
+let cityDate = document.getElementById('city-date');
+let iconEl = document.getElementById('icon');
+let tempEl = document.getElementById('temp');
+let windEl = document.getElementById('wind');
+let humidityEl = document.getElementById('humidity');
+let forecastDate = document.getElementById('forecast-date');
+let tempForecast = document.getElementById('forecast-temp');
+let windForecast = document.getElementById('forecast-wind');
+let forecastHumidity = document.getElementById('forecast-humidity');
 
 // getting date
 
 const date = new Date();
 var dateString = date.toLocaleDateString();
-//console.log(date)
-//console.log(dateString)
 
 // URL for current day parameters (city name + weather units of measurements)
-//let geoCodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
 
-//let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
+function getWeather(city) {
+let geoCodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=' + apiKey; 
+    fetch(geoCodingUrl)
+    .then(function (response){
+        return response.json(); 
+    })
+    .then (function(data){
+        console.log(data)
+        let lat = data[0].lat
+        let lon =  data[0].lon;
+        getCurrentWeather(lon, lat);
+        getForecast(lon, lat);
+    })
+}
 
-//let forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial&appid=' + apiKey;
+function getCurrentWeather(longitude, latitude){
+    let currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid='  + apiKey;
+
+    fetch(currentWeatherUrl)
+    .then(function (response){
+        return response.json(); 
+    })
+    .then (function(data){
+        console.log(data);
+        let tempData = data.main.temp;
+        let windData = data.wind.speed;
+        let humidityData = data.main.humidity;
+        let cityAndDateData = data.name + ' ('+ dateString + ')';
+        cityDate.textContent = cityAndDateData;
+        tempEl.textContent = 'Temp: ' + tempData + ' °F';
+        windEl.textContent = 'Wind: ' + windData + ' MPH';
+        humidityEl.textContent = 'Humidity: ' + humidityData + ' %';
+    })
+} 
+
+function getForecast(longitude, latitude){
+    let forecastWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid='  + apiKey;
+
+    fetch(forecastWeatherUrl)
+    .then(function (response){
+        return response.json(); 
+    })
+    .then (function(data){
+        console.log(data)
+        let dateData = data.list[8]['dt_txt'].split(' ')[0];
+        forecastDate.textContent = dateData;
+        
+        console.log(dateData) 
+        let weatherIcon = data.list[8].weather[0].icon;
+        let weatherIconLink = 'https://openweathermap.org/img/w/' + weatherIcon + '.png';
+            iconEl.src = weatherIconLink;
+            iconEl.textContent = weatherIconLink;
+        let forecastTemp =  data.list[8].main.temp;
+            tempForecast.textContent = 'Temp: ' + forecastTemp + ' °F';
+        let forecastWind = data.list[8].wind.speed;
+            windForecast.textContent = 'wind' + forecastWind + 'MPH';
+        let humidityForecast = data.list[8].main.humidity;
+        forecastHumidity.textContent = 'Humidity' + humidityForecast + '%'
+            
+        
+        
+
+console.log(weatherIconLink);
+    })
+
+}
+
 
 // get search history in local storage
 function getSearchHistory() {
@@ -44,7 +114,7 @@ function setSearchHistory(city) {
     }
 
     if (!parseHistory.includes(city)) {
-        parseHistory.push(city);
+        parseHistory.unshift(city);
         window.localStorage.setItem('cityHistory', JSON.stringify(parseHistory))
     }
 }
@@ -62,7 +132,7 @@ function displayHistory() {
             searchHistory.append(liEl);
         }
     }
-
+    inputEl.value = '';
 }
 console.log(searchBtn)
 searchBtn.addEventListener('click', function(e){
@@ -71,10 +141,9 @@ searchBtn.addEventListener('click', function(e){
     var city = inputEl.value;
     setSearchHistory(city);
     displayHistory(); 
-    // set city to searchHistory 
-
-    // display search history, 
-    // clear input
-    // get weather data.
     
+    getWeather(city);
 })
+
+displayHistory();
+
